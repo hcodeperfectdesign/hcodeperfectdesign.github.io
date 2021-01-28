8c53f839383477b12f0720eadfea478aa3261199
jovair <jvr_ribeirolive.com>
2021-01-27 22:21:24 -0300

    Alteração na instrução JavaScript

--- assets/js/openMenu.js
+++ assets/js/openMenu.js
@@ -1,13 +1,12 @@
-    const btnOpen = document.querySelector('#open-btn');
-        btnOpen.addEventListener('click', e=> {
-        const header = document.querySelector('body');
-        header.classList.toggle('open-menu');
-        }
-    );
-    
-    const btnClose = document.querySelector('#header-menu');
-        btnClose.addEventListener('click', e=> {
-        const header = document.querySelector('body');
-        header.classList.toggle('open-menu');
-        }
-    );
+const btnOpen = document.querySelector('#open-btn');
+    btnOpen.addEventListener('click', e=> {
+    const header = document.querySelector('body');
+    header.classList.toggle('open-menu');
+    }
+);
+const btnClose = document.querySelector('#close-btn');
+    btnClose.addEventListener('click', e=> {
+    const header = document.querySelector('body');
+    header.classList.toggle('open-menu');
+    }
+);
